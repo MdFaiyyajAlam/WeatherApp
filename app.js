0bab6291c28d1a79379e620c07ae5c17fc0cebb5
Md Faiyyaj Alam <mdfaiyyajalam54918@gmail.com>
2023-01-23 16:16:47 +0530

    index

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 8000;
 
 
 // public static path
-const static_path = path.join(__dirname, "../public");
-const template_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
+const static_path = path.join(__dirname, "./public");
+const template_path = path.join(__dirname, "./templates/views");
+const partials_path = path.join(__dirname, "./templates/partials");
 
 app.set('view engine', 'hbs');
 app.set('views', template_path);
